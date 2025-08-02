a9b46676709d9547e3716474dfe7ea4c7478345a
ksujan12 <ksujan0112@gmail.com>
2025-08-02 14:21:34 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello from React!</h1>
+      <p>This is a basic webpage with React, JavaScript, HTML, and CSS.</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
